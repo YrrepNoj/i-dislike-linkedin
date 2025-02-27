cbac18497d4e39c00af7a175f87ba1efbf436f5d
Jon Perry <yrrepnoj@gmail.com>
2025-02-26 19:57:02 -0500

    chore: clean up default pngs and svgs

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,15 +1,6 @@
 import pkg from "../package.json";
 
 const manifest = {
-  action: {
-    default_icon: {
-      16: "icons/16.png",
-      19: "icons/19.png",
-      32: "icons/32.png",
-      38: "icons/38.png",
-    },
-    default_popup: "src/entries/popup/index.html",
-  },
   background: {
     service_worker: "src/entries/background/main.ts",
   },
@@ -21,20 +12,7 @@ const manifest = {
   ],
   host_permissions: ["*://*/*"],
   icons: {
-    16: "icons/16.png",
-    19: "icons/19.png",
-    32: "icons/32.png",
-    38: "icons/38.png",
-    48: "icons/48.png",
-    64: "icons/64.png",
-    96: "icons/96.png",
-    128: "icons/128.png",
-    256: "icons/256.png",
-    512: "icons/512.png",
-  },
-  options_ui: {
-    page: "src/entries/options/index.html",
-    open_in_tab: true,
+    256: "icons/thumbs-down-o.256x256.png"
   },
 };
 
